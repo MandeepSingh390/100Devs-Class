d051a64813e13a611a3be9d8d17f57496263df5d
Samurai <mandeepprof@outlook.com>
2022-03-09 11:03:16 +0530

    did class 16 material

--- class16-materials/bring-it-on-again/js/main.js
+++ class16-materials/bring-it-on-again/js/main.js
@@ -1,17 +1,53 @@
 // *Variables*
 // Declare a variable, assign it a value, and alert the value
-
+let var1 = 69
+alert(var1)
 // Create a variable, divide it by 10, and console log the value
-
+let var2 = 1000
+console.log(var2/500)
 // *Functions*
 // Create a function that multiplys 3 numbers and alerts the product
-
+function multiThree(num1,num2,num3){
+	alert(num1*num2*num3)
+}
+multiThree(5,3,2)
 // Create a function that takes in 4 numbers. Add the first two numbers and subtract the next two. Console log the result
-
+function subTwo(num1,num2,num3,num4){
+	console.log(num1+num2)
+	console.log(num3-num4)
+}
+subTwo(5,1,6,3)
 // *Conditionals*
 // Create a function that takes in 3 numbers. Starting with 100 add the first number, subtract the second, and divide the third. If the value is greater then 25, console log "WE HAVE A WINNNA"
-
+function divThree(num1,num2,num3){
+	let var3 = 100
+	var3 += num1
+	var3 -+ num2
+	var3 /= num3
+	if (var3 > 25){
+		console.log("WE HAVE A WINNNA")
+	}
+}
+divThree(1000,448,2)
 // Create a function that takes in a day of the week. If it is a weekend alert, "weekend" and if not alert "week day". Handle capitilization and if the user does not enter a day of the week alert "Try again!"
-
+function takesTwo(str){
+	str = str.toLowerCase()
+	if (str == 'sunday' || str == 'saturday' || str == 'friday'){
+		console.log("weekend")
+	}
+	else if(str == 'monday' || str == 'tuesday' || str == 'wednesday' || str == 'thrusday'){
+		console.log('WeekDay')
+	}
+	else{
+		console.log('Please Enter a proper day')
+	}
+}
+takesTwo('Tuesday')
 //*Loops*
 //Create a function that takes in a number. Console log all values from 1 to that number or greater, but count by 3
+function takesOne(num1){
+	for (let i = 0; i <= num1; i+=3) {
+		console.log(i)
+	}
+}
+takesOne(69)
--- class16-materials/bring-it-on/js/main.js
+++ class16-materials/bring-it-on/js/main.js
@@ -4,16 +4,40 @@ let var1 = 69
 console.log(var1)
 // Create a variable, add 10 to it, and alert the value
 let var2 = 15
-console.log(var2 + 10)
+alert(var2 + 10)
 // *Functions*
 // Create a function that subtracts 4 numbers and alerts the difference
-
+function subFour(num1,num2,num3,num4){
+	alert(num1-num2-num3-num4)
+}
+subFour(1000,500,250,125)
 // Create a function that divides one number by another and returns the remainder
-
+function divideFour(num1,num2){
+	return(num1%num2)
+}
+console.log(divideFour(5,2))
 // *Conditionals*
 // Create a function that adds two numbers and if the sum is greater than 50 alert Jumanji
-
+function sumTwo(num1,num2){
+	let sum = num1 + num2
+	if (sum > 50){
+		alert("Jumanji")
+	}
+}
+sumTwo(49,2)
 // Create a function that multiplys three numbers and if the product is divisible by 3 alert ZEBRA
-
+function mulThree(num1,num2,num3){
+	let product = num1 * num2 * num3
+	if (product % 3 == 0){
+		alert("Zebra")
+	}
+}
+mulThree(3,2,1)
 //*Loops*
 //Create a function that takes in a word and a number. Console log the word x times where x was the number passed in
+function wordNum(str,num1){
+	for (let i=1; i < num1; i++){
+		console.log(str)
+	}
+}
+wordNum("bob",6)
