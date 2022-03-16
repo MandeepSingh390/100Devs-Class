f5d585f06e5e9e277b4c6ec8545e0f675cc2a5db
Samurai <mandeepprof@outlook.com>
2022-03-16 19:17:59 +0530

    test2

--- class19-materials/arrays-fight-to-the-finish/js/main.js
+++ class19-materials/arrays-fight-to-the-finish/js/main.js
@@ -1,7 +1,7 @@
 //Create an array of movie titles. Loop through the array and each element to the h2.
-
-
+let movieArray = ['cars1','cars2','cars3']
+movieArray.forEach((x)=>document.querySelector('h2').innerText += " "(x))
 //Create an array of numbers. Loop through the array and three to each number and replace the old number.
-
+let numberArray = [1,2,3,4,5]
 
 //Find the average of all the numbers from question three
