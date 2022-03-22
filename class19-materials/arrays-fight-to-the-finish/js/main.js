c33aa4b3e46a0e8ab6e68dcfbd41919101782e5f
Samurai <mandeepmandla11@gmail.com>
2022-03-23 04:56:26 +0530

    class 19tasks and class 21 materials

--- class19-materials/arrays-fight-to-the-finish/js/main.js
+++ class19-materials/arrays-fight-to-the-finish/js/main.js
@@ -1,7 +1,19 @@
 //Create an array of movie titles. Loop through the array and each element to the h2.
 let movieArray = ['cars1','cars2','cars3']
-movieArray.forEach((x)=>document.querySelector('h2').innerText += " "(x))
+movieArray.forEach((x)=>document.querySelector('h2').innerText += " " + (x))
 //Create an array of numbers. Loop through the array and three to each number and replace the old number.
 let numberArray = [1,2,3,4,5]
-
+// for (let i=0;i<numberArray.length;i++){
+// 	numberArray[i] += 3
+// }
+numberArray.forEach((item, i) => {
+	numberArray[i] = item + 3
+})
+console.log(numberArray)
 //Find the average of all the numbers from question three
+let newNum = 0
+for (let i=0;i<numberArray.length;i++){
+	newNum += numberArray[i]
+	// console.log(newNum)
+}
+console.log(newNum)
--- class19-materials/arrays-in-it-to-win-it/js/main.js
+++ class19-materials/arrays-in-it-to-win-it/js/main.js
@@ -2,6 +2,10 @@
 let array1 =  ["Demon Slayer Mugen Train", "Rush Hour", "Shawshank Remdemption"]
 //Using the array from above, store the first movie in a variable
 let var1 = array1[0]
+console.log(var1)
 //Get the length of the original array and store it in a new variable
-let var2 = length(array1[0])
+let var2 = array1.length
+console.log(var2)
 //Get the last element in that array and store it in a new variable. What if your array was really large and you didn't know the last index? Would your solution still work?
+let var3 = array1[array1.length - 1]
+console.log(var3)
